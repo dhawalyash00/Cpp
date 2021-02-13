47215b2a090d87a341043a75e74edc01ee601fa7
dhawalyash <71340041+dhawalyash00@users.noreply.github.com>
2021-02-13 20:38:14 +0530

    Create function1.cpp

---
+++ Function/function1.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+int fun(int, int);
+int main(){
+    int a, b;
+    cout<<"enter 2 numbers\n";
+    cin>>a>>b;
+    cout<<"sum is"<<" "<<fun(a,b);
+
+
+
+
+}int fun(int x, int y){
+
+return(x+y);
+}
